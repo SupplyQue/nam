7adb94a415e62efa441c0b212d27347dd7716c5d
NamrathaKaranth <53632761+NamrathaKaranth@users.noreply.github.com>
2020-08-02 01:12:13 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
+if (process.env.NODE_env !== 'production') {
+    require('dotenv').config()
+}
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routee/index')
-const { MongoClient } = require('mongodb')
+const { param } = require('./routee/index')
 
-async function main() {
-    const uri = "mongodb+srv://namrathakaranth:karanth1@mydb.slfpn.mongodb.net/mydb?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-        await client.connect();
-        await listDatabases(client);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-main().catch(console.error)
 
-async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', error => console.error('connnected to mongoose'))
 
 app.use('/', indexRouter)
 app.listen(process.env.Port || 3000)
